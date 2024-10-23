b258ac29e600e9f0fc046e46b09b130bffaa4d59
xd-Abi <abinayansureskumar@gmail.com>
2024-10-23 14:16:36 +0200

    Loading measurements from datacake api

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,37 @@
 use dotenv::dotenv;
+use reqwest::header::HeaderMap;
+use reqwest::Client;
+use serde_json::json;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let api_key = env::var("DATACAKE_KEY").expect("API_KEY not found in .env");
+
+    let api_key = env::var("DATACAKE_KEY").expect("DATACAKE_KEY not found in .env");
     let device_id = env::var("DEVICE_ID").expect("DEVICE_ID not found in .env");
+    let client = Client::builder().build()?;
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Authorization", format!("Token {}", api_key).parse()?);
+    headers.insert("Content-Type", "application/json".parse()?);
+
+    let query = json!({
+        "query": "query($deviceId:String!){device(deviceId:$deviceId){currentMeasurements(allActiveFields:true){value field{verboseFieldName fieldName}modified}}}",
+        "variables": {
+            "deviceId": device_id
+        }
+    });
+
+    let request = client
+        .request(reqwest::Method::POST, "https://api.datacake.co/graphql/")
+        .headers(headers)
+        .json(&query);
+
+    let response = request.send().await?;
+    let body = response.text().await?;
+
+    println!("{}", body);
+
+    Ok(())
 }
